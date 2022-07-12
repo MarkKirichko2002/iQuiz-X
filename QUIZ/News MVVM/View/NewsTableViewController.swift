b4f7b6b06fb5f73dc837102933608d602b56c6d2
MarkDestroyer <markkir123@gmail.com>
2022-07-12 19:52:51 +0300

    Update
    Added Data Binding in MVVM for News.

--- QUIZ/News MVVM/View/NewsTableViewController.swift
+++ QUIZ/News MVVM/View/NewsTableViewController.swift
@@ -6,36 +6,59 @@
 //
 
 import UIKit
-import SCLAlertView
+import Alamofire
+import SDWebImage
+
 
 class NewsTableViewController: UITableViewController {
     
-    var newsViewModel = NewsViewModel()
-    var newsArray: [Article] = []
-    var newscellmodel = NewsTableViewCellModel()
+    var newsViewModel = NewsListViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        newsViewModel.GetNews { [weak self] news in
-            guard let self = self else { return }
-            self.newsArray = news
-            self.tableView.reloadData()
-            print(news)
+        newsViewModel.news.bind { [weak self] _ in
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+            }
         }
+        GetNews()
     }
     
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return newsArray.count
+        return newsViewModel.news.value?.count ?? 0
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: NewsTableViewCell.identifier, for: indexPath) as! NewsTableViewCell
         
-        newscellmodel.configure(article: newsArray[indexPath.row], image: cell.NewsImage, title: cell.TitleLabel, description: cell.DescriptionLabel)
+        cell.NewsImage.sd_setImage(with: URL(string: newsViewModel.news.value?[indexPath.row].urlToImage ?? ""))
+        cell.TitleLabel.text = newsViewModel.news.value?[indexPath.row].title ?? ""
+        cell.DescriptionLabel.text = newsViewModel.news.value?[indexPath.row].description ?? ""
         
         return cell
     }
     
+    func GetNews() {
+        
+        AF.request("https://newsapi.org/v2/top-headlines?country=ru&category=technology&apiKey=c6fb14909d524ae68ea631e5cb55ae67").responseData { response in
+                  
+            print(response.request as Any)
+            print(response.response as Any)
+        
+            guard let data = response.data else { return }
+            print(data)
+            
+            var newsresponse: NewsResponse?
+            do {
+                newsresponse = try JSONDecoder().decode(NewsResponse.self, from: data)
+                self.newsViewModel.news.value = newsresponse?.articles.compactMap({
+                    NewsTableViewCellViewModel(urlToImage: $0.urlToImage ?? "", url: $0.url ?? "", description: $0.description ?? "", title: $0.title ?? "")
+                })
+            } catch {
+                print(error)
+            }
+        }
+    }
 }
