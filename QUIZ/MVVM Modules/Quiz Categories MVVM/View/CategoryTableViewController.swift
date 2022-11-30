ee0442b1a2e2b97ae66ab8ff18b3997c4e28aeeb
MarkDestroyer <markkir123@gmail.com>
2022-11-30 22:36:52 +0300

    update

--- QUIZ/MVVM Modules/Quiz Categories MVVM/View/CategoryTableViewController.swift
+++ QUIZ/MVVM Modules/Quiz Categories MVVM/View/CategoryTableViewController.swift
@@ -39,24 +39,10 @@ final class CategoryTableViewController: UITableViewController {
         }
         
         switch(indexPath.row) {
-        case 0: categories.GoToStart(quiz: QuizPlanets(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 1: categories.GoToStart(quiz: QuizHistory(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 2: categories.GoToStart(quiz: QuizAnatomy(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 3: categories.GoToStart(quiz: QuizSport(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 4: categories.GoToStart(quiz: QuizGames(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 5: categories.GoToStart(quiz: QuizIQ(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 6: categories.GoToStart(quiz: QuizEconomy(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 7: categories.GoToStart(quiz: QuizGeography(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 8: categories.GoToStart(quiz: QuizEcology(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 9: categories.GoToStart(quiz: QuizPhysics(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 10: categories.GoToStart(quiz: QuizChemistry(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 11: categories.GoToStart(quiz: QuizInformatics(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 12: categories.GoToStart(quiz: QuizLiterature(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 13: categories.GoToStart(quiz: QuizRoadTraffic(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 14: categories.GoToStart(quiz: QuizSwift(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 15: categories.GoToStart(quiz: QuizUnderwater(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 16: categories.GoToStart(quiz: QuizChess(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 17: categories.GoToStart(quiz: QuizHalloween(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
+        
+          case  0...17:
+            categories.GoToStart(quiz: categories.categories[indexPath.row].base, category: categories.categories[indexPath.row], storyboard: self.storyboard, view: self.view)
+            
         case 18: categories.PresentRandomQuiz(storyboard: storyboard, view: view)
             
         default: break
--- QUIZ/MVVM Modules/Quiz Tasks MVVM/View/TasksTableViewController.swift
+++ QUIZ/MVVM Modules/Quiz Tasks MVVM/View/TasksTableViewController.swift
@@ -35,31 +35,17 @@ final class TasksTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        player.Sound(resource: tasks.tasks[indexPath.row].sound)
+        player.Sound(resource: categories.categories[indexPath.row].sound)
         
-        if let cell = tableView.cellForRow(at: indexPath) as? CategoryTableViewCell {
-            cell.didSelect(indexPath: indexPath)
+        if let cell = tableView.cellForRow(at: indexPath) as? TasksTableViewCell {
+               cell.didSelect(indexPath: indexPath)
         }
         
         switch(indexPath.row) {
-        case 0: categories.GoToStart(quiz: QuizPlanets(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 1: categories.GoToStart(quiz: QuizHistory(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 2: categories.GoToStart(quiz: QuizAnatomy(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 3: categories.GoToStart(quiz: QuizSport(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 4: categories.GoToStart(quiz: QuizGames(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 5: categories.GoToStart(quiz: QuizIQ(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 6: categories.GoToStart(quiz: QuizEconomy(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 7: categories.GoToStart(quiz: QuizGeography(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 8: categories.GoToStart(quiz: QuizEcology(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 9: categories.GoToStart(quiz: QuizPhysics(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 10: categories.GoToStart(quiz: QuizChemistry(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 11: categories.GoToStart(quiz: QuizInformatics(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 12: categories.GoToStart(quiz: QuizLiterature(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 13: categories.GoToStart(quiz: QuizRoadTraffic(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 14: categories.GoToStart(quiz: QuizSwift(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 15: categories.GoToStart(quiz: QuizUnderwater(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 16: categories.GoToStart(quiz: QuizChess(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
-        case 17: categories.GoToStart(quiz: QuizHalloween(), category: categories.categories[indexPath.row], storyboard: storyboard, view: view)
+        
+          case  0...17:
+            categories.GoToStart(quiz: categories.categories[indexPath.row].base, category: categories.categories[indexPath.row], storyboard: self.storyboard, view: self.view)
+            
         case 18: categories.PresentRandomQuiz(storyboard: storyboard, view: view)
             
         default: break
