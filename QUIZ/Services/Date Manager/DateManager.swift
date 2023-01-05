0e479f9b3b502c2a2bf1a12508cfc29a251b108d
MarkDestroyer <markkir123@gmail.com>
2023-01-05 23:29:41 +0300

    update

---
+++ QUIZ/Services/Date Manager/DateManager.swift
@@ -0,0 +1,22 @@
+//
+//  DateManager.swift
+//  QUIZ
+//
+//  Created by Марк Киричко on 05.01.2023.
+//
+
+import Foundation
+
+class DateManager {
+    
+    let date = Date()
+    
+    func GetCurrentDate()-> String {
+        var currentDate = ""
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        currentDate = dateFormatter.string(from: date)
+        return currentDate
+    }
+    
+}
