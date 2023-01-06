e8e62b91a905718542447d4d44e984d87d2d51ea
MarkDestroyer <markkir123@gmail.com>
2023-01-06 16:40:30 +0300

    update
    Added option to generate random articles on news screen.

--- QUIZ/Modules/MVVM Modules/News Module/News MVVM Event Bus/ViewModel/NewsListViewModel.swift
+++ QUIZ/Modules/MVVM Modules/News Module/News MVVM Event Bus/ViewModel/NewsListViewModel.swift
@@ -10,11 +10,13 @@ import Alamofire
 
 struct NewsListViewModel {
     
-    public var news: [Article] = []
+    var news: [Article] = []
+    private let urls = ["https://newsapi.org/v2/top-headlines?country=ru&category=general&apiKey=c6fb14909d524ae68ea631e5cb55ae67","https://newsapi.org/v2/top-headlines?country=ru&category=science&apiKey=c6fb14909d524ae68ea631e5cb55ae67", "https://newsapi.org/v2/top-headlines?country=ru&category=technology&apiKey=c6fb14909d524ae68ea631e5cb55ae67", "https://newsapi.org/v2/top-headlines?country=ru&category=sport&apiKey=c6fb14909d524ae68ea631e5cb55ae67","https://newsapi.org/v2/top-headlines?country=ru&category=business&apiKey=c6fb14909d524ae68ea631e5cb55ae67", "https://newsapi.org/v2/top-headlines?country=ru&category=entertainment&apiKey=c6fb14909d524ae68ea631e5cb55ae67"]
+    private var url = "https://newsapi.org/v2/top-headlines?country=ru&category=science&apiKey=c6fb14909d524ae68ea631e5cb55ae67"
+    private let player = SoundClass()
     
-    public func GetNews() {
-        let url = "https://newsapi.org/v2/top-headlines?country=ru&category=technology&apiKey=c6fb14909d524ae68ea631e5cb55ae67"
-        
+    func GetNews() {
+       
         AF.request(url).response { data in
             guard let data = data.data else {return}
         
@@ -30,5 +32,34 @@ struct NewsListViewModel {
             }
             Bus.shared.publish(type: .newsFetch, event: event)
         }
+    }
+    
+    mutating func GenerateRandomNews() {
+        self.url = urls.randomElement() ?? ""
+        player.PlaySound(resource: "dice.wav")
+        switch url {
+            
+        case "https://newsapi.org/v2/top-headlines?country=ru&category=general&apiKey=c6fb14909d524ae68ea631e5cb55ae67":
+            player.PlaySound(resource: "literature.mp3")
+          
+        case "https://newsapi.org/v2/top-headlines?country=ru&category=science&apiKey=c6fb14909d524ae68ea631e5cb55ae67":
+            player.PlaySound(resource: "chemistry.mp3")
+            
+        case "https://newsapi.org/v2/top-headlines?country=ru&category=technology&apiKey=c6fb14909d524ae68ea631e5cb55ae67":
+            player.PlaySound(resource: "technology.wav")
+            
+        case "https://newsapi.org/v2/top-headlines?country=ru&category=sport&apiKey=c6fb14909d524ae68ea631e5cb55ae67":
+            player.PlaySound(resource: "sport.mp3")
+            
+        case "https://newsapi.org/v2/top-headlines?country=ru&category=business&apiKey=c6fb14909d524ae68ea631e5cb55ae67":
+            player.PlaySound(resource: "economics.mp3")
+            
+        case "https://newsapi.org/v2/top-headlines?country=ru&category=entertainment&apiKey=c6fb14909d524ae68ea631e5cb55ae67":
+            player.PlaySound(resource: "entertainment.mp3")
+            
+        default:
+            break
+        }
+        GetNews()
     }
 }
