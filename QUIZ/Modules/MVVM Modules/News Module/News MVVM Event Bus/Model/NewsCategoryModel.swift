113e2bed03bed1b28a20850834989952aaa815f7
MarkDestroyer <markkir123@gmail.com>
2023-01-07 00:03:48 +0300

    update
    Added screen for selecting news category.

---
+++ QUIZ/Modules/MVVM Modules/News Module/News MVVM Event Bus/Model/NewsCategoryModel.swift
@@ -0,0 +1,15 @@
+//
+//  NewsCategoryModel.swift
+//  QUIZ
+//
+//  Created by Марк Киричко on 06.01.2023.
+//
+
+import Foundation
+
+struct NewsCategoryModel {
+    var id: Int
+    var name: String
+    var icon: String
+    var sound: String
+}
--- QUIZ/Modules/MVVM Modules/News Module/News MVVM Event Bus/ViewModel/NewsListViewModel.swift
+++ QUIZ/Modules/MVVM Modules/News Module/News MVVM Event Bus/ViewModel/NewsListViewModel.swift
@@ -11,10 +11,11 @@ import Alamofire
 struct NewsListViewModel {
     
     var news: [Article] = []
-    private let urls = ["https://newsapi.org/v2/top-headlines?country=ru&category=general&apiKey=c6fb14909d524ae68ea631e5cb55ae67","https://newsapi.org/v2/top-headlines?country=ru&category=science&apiKey=c6fb14909d524ae68ea631e5cb55ae67", "https://newsapi.org/v2/top-headlines?country=ru&category=technology&apiKey=c6fb14909d524ae68ea631e5cb55ae67", "https://newsapi.org/v2/top-headlines?country=ru&category=sport&apiKey=c6fb14909d524ae68ea631e5cb55ae67","https://newsapi.org/v2/top-headlines?country=ru&category=business&apiKey=c6fb14909d524ae68ea631e5cb55ae67", "https://newsapi.org/v2/top-headlines?country=ru&category=entertainment&apiKey=c6fb14909d524ae68ea631e5cb55ae67"]
-    private var url = "https://newsapi.org/v2/top-headlines?country=ru&category=science&apiKey=c6fb14909d524ae68ea631e5cb55ae67"
     private let player = SoundClass()
+    private let urls = ["https://newsapi.org/v2/top-headlines?country=ru&category=general&apiKey=c6fb14909d524ae68ea631e5cb55ae67","https://newsapi.org/v2/top-headlines?country=ru&category=science&apiKey=c6fb14909d524ae68ea631e5cb55ae67", "https://newsapi.org/v2/top-headlines?country=ru&category=technology&apiKey=c6fb14909d524ae68ea631e5cb55ae67", "https://newsapi.org/v2/top-headlines?country=ru&category=sport&apiKey=c6fb14909d524ae68ea631e5cb55ae67","https://newsapi.org/v2/top-headlines?country=ru&category=business&apiKey=c6fb14909d524ae68ea631e5cb55ae67"]
+    private var url = "https://newsapi.org/v2/top-headlines?country=ru&category=science&apiKey=c6fb14909d524ae68ea631e5cb55ae67"
     
+    // получить новости
     func GetNews() {
        
         AF.request(url).response { data in
@@ -34,6 +35,41 @@ struct NewsListViewModel {
         }
     }
     
+    mutating func CurrentCategory(category: NewsCategoryModel) {
+        switch category.id {
+            
+        case 1:
+            url = "https://newsapi.org/v2/top-headlines?country=ru&category=general&apiKey=c6fb14909d524ae68ea631e5cb55ae67"
+            player.PlaySound(resource: "literature.mp3")
+            GetNews()
+            
+        case 2:
+            url = "https://newsapi.org/v2/top-headlines?country=ru&category=technology&apiKey=c6fb14909d524ae68ea631e5cb55ae67"
+            player.PlaySound(resource: "technology.wav")
+            GetNews()
+            
+        case 3:
+            url = "https://newsapi.org/v2/top-headlines?country=ru&category=sport&apiKey=c6fb14909d524ae68ea631e5cb55ae67"
+            player.PlaySound(resource: "sport.mp3")
+            GetNews()
+            
+        case 4:
+            url = "https://newsapi.org/v2/top-headlines?country=ru&category=business&apiKey=c6fb14909d524ae68ea631e5cb55ae67"
+            player.PlaySound(resource: "economics.mp3")
+            GetNews()
+        
+        case 5:
+            url = "https://newsapi.org/v2/top-headlines?country=ru&category=science&apiKey=c6fb14909d524ae68ea631e5cb55ae67"
+            player.PlaySound(resource: "chemistry.mp3")
+            GetNews()
+            
+        default:
+            break
+            
+        }
+    }
+    
+    // сгенерировать случайные статьи
     mutating func GenerateRandomNews() {
         self.url = urls.randomElement() ?? ""
         player.PlaySound(resource: "dice.wav")
@@ -42,9 +78,6 @@ struct NewsListViewModel {
         case "https://newsapi.org/v2/top-headlines?country=ru&category=general&apiKey=c6fb14909d524ae68ea631e5cb55ae67":
             player.PlaySound(resource: "literature.mp3")
           
-        case "https://newsapi.org/v2/top-headlines?country=ru&category=science&apiKey=c6fb14909d524ae68ea631e5cb55ae67":
-            player.PlaySound(resource: "chemistry.mp3")
-            
         case "https://newsapi.org/v2/top-headlines?country=ru&category=technology&apiKey=c6fb14909d524ae68ea631e5cb55ae67":
             player.PlaySound(resource: "technology.wav")
             
@@ -53,9 +86,9 @@ struct NewsListViewModel {
             
         case "https://newsapi.org/v2/top-headlines?country=ru&category=business&apiKey=c6fb14909d524ae68ea631e5cb55ae67":
             player.PlaySound(resource: "economics.mp3")
-            
-        case "https://newsapi.org/v2/top-headlines?country=ru&category=entertainment&apiKey=c6fb14909d524ae68ea631e5cb55ae67":
-            player.PlaySound(resource: "entertainment.mp3")
+        
+        case "https://newsapi.org/v2/top-headlines?country=ru&category=science&apiKey=c6fb14909d524ae68ea631e5cb55ae67":
+            player.PlaySound(resource: "chemistry.mp3")
             
         default:
             break
