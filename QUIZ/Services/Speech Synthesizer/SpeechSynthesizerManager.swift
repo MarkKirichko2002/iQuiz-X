63fdd2fdcef97563c23b5372e1553c49a5fbaf19
MarkDestroyer <markkir123@gmail.com>
2023-01-07 18:01:13 +0300

    update
    Added tap gesture for VoiceCommandLabel.

---
+++ QUIZ/Services/Speech Synthesizer/SpeechSynthesizerManager.swift
@@ -0,0 +1,23 @@
+//
+//  SpeechSynthesizerManager.swift
+//  QUIZ
+//
+//  Created by Марк Киричко on 07.01.2023.
+//
+
+import AVFoundation
+
+class SpeechSynthesizerManager {
+    
+    private let synthesizer = AVSpeechSynthesizer()
+    
+    func sayComment(comment: String) {
+        let utterance = AVSpeechUtterance(string: comment)
+        utterance.voice = AVSpeechSynthesisVoice(language: "ru-RU")
+        synthesizer.speak(utterance)
+    }
+    
+    func StopSaying() {
+        synthesizer.stopSpeaking(at: .immediate)
+    }
+}
