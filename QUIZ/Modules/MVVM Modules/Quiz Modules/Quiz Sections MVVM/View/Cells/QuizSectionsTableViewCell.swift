2421347de67eb408167a30c6df50a8824cff4d94
MarkDestroyer <markkir123@gmail.com>
2023-01-18 21:02:33 +0300

    update
    Added info button on quiz sections screen.

--- QUIZ/Modules/MVVM Modules/Quiz Modules/Quiz Sections MVVM/View/Cells/QuizSectionsTableViewCell.swift
+++ QUIZ/Modules/MVVM Modules/Quiz Modules/Quiz Sections MVVM/View/Cells/QuizSectionsTableViewCell.swift
@@ -6,23 +6,48 @@
 //
 
 import UIKit
+import SCLAlertView
 
 class QuizSectionsTableViewCell: UITableViewCell {
 
     static let identifier = "QuizSectionsTableViewCell"
     private let animation = AnimationClass()
+    private var section: QuizSectionModel?
+    var vc: UIViewController?
     
     @IBOutlet weak var QuizSectionIcon: RoundedImageView!
     @IBOutlet weak var QuizSectionCategoryName: UILabel!
     @IBOutlet weak var QuizSectionItemCount: UILabel!
     @IBOutlet weak var PercentageLabel: UILabel!
+    @IBOutlet weak var InfoButton: UIButton!
+    
+    @IBAction func ShowInfo() {
+        animation.springButton(button: self.InfoButton)
+        if let section = section {
+            ShowAlert(message: section.name, info: section.info)
+        }
+    }
+    
+    private func ShowAlert(message: String, info: String) {
+        
+        let dialogMessage = UIAlertController(title: message, message: info, preferredStyle: .alert)
+         
+         let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+             
+          })
+         
+         dialogMessage.addAction(ok)
+         
+        self.vc?.present(dialogMessage, animated: true, completion: nil)
+    }
     
     func configure(section: QuizSectionModel) {
         QuizSectionIcon.image = UIImage(named: section.icon)
         QuizSectionIcon.sound = section.sound
         QuizSectionCategoryName.text = section.name
-        QuizSectionItemCount.text = "пройдено: \(section.itemsCount)/20"
+        QuizSectionItemCount.text = "пройдено: \(section.itemsCount)/19"
         PercentageLabel.text = "итог: \(section.percentage)%"
+        self.section = section
     }
     
     func didSelect(indexPath: IndexPath) {
