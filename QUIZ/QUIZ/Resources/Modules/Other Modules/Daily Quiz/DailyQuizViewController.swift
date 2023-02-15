ba7c009e02e8cc461f21d2202d7ae0716baf5d26
MarkDestroyer <markkir123@gmail.com>
2023-02-15 22:49:34 +0300

    update

--- QUIZ/QUIZ/Resources/Modules/Other Modules/Daily Quiz/DailyQuizViewController.swift
+++ QUIZ/QUIZ/Resources/Modules/Other Modules/Daily Quiz/DailyQuizViewController.swift
@@ -5,7 +5,6 @@
 //  Created by Марк Киричко on 06.01.2023.
 //
 
-import Foundation
 import UIKit
 
 final class DailyQuizViewController: UIViewController {
@@ -14,23 +13,63 @@ final class DailyQuizViewController: UIViewController {
     private let dateManager = DateManager()
     private let quizCategoriesViewModel = QuizCategoriesViewModel()
     
-    @IBOutlet weak var DailyQuizCategoryIcon: RoundedImageView!
-    @IBOutlet weak var DailyQuizCategoryName: UILabel!
-    @IBOutlet weak var CurrentDateLabel: UILabel!
+    private let DailyQuizCategoryIcon: RoundedImageView = {
+        let icon = RoundedImageView()
+        icon.color = UIColor.white
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        return icon
+    }()
+    
+    private let DailyQuizCategoryName: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor.white
+        label.font = .systemFont(ofSize: 28, weight: .black)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let CurrentDateLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor.white
+        label.font = .systemFont(ofSize: 28, weight: .black)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubviews(DailyQuizCategoryIcon, DailyQuizCategoryName, CurrentDateLabel)
         self.quizCategoriesViewModel.view = self.view
         self.quizCategoriesViewModel.storyboard = self.storyboard
+        SetUpConstraints()
+        DisplayDailyCategory()
+    }
+    
+    private func SetUpConstraints() {
+        NSLayoutConstraint.activate([
+            
+            DailyQuizCategoryIcon.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            DailyQuizCategoryIcon.widthAnchor.constraint(equalToConstant: 200),
+            DailyQuizCategoryIcon.heightAnchor.constraint(equalToConstant: 200),
+            
+            DailyQuizCategoryName.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            DailyQuizCategoryName.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            DailyQuizCategoryName.heightAnchor.constraint(equalToConstant: 30),
+            DailyQuizCategoryName.topAnchor.constraint(equalTo: DailyQuizCategoryIcon.bottomAnchor, constant: 40),
+            
+            CurrentDateLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            CurrentDateLabel.heightAnchor.constraint(equalToConstant: 30),
+            CurrentDateLabel.topAnchor.constraint(equalTo: DailyQuizCategoryName.bottomAnchor, constant: 40)
+        ])
+    }
+    
+    private func DisplayDailyCategory() {
         if let category = self.category {
             view.backgroundColor = UIColor(patternImage: UIImage(named: category.background)!)
             DailyQuizCategoryIcon.image = UIImage(named: category.image)
             DailyQuizCategoryIcon.sound = category.sound
-            DailyQuizCategoryIcon.color = UIColor.white
             DailyQuizCategoryName.text = category.name
-            DailyQuizCategoryName.textColor = UIColor.white
             CurrentDateLabel.text = "сегодня: \(dateManager.GetCurrentDate())"
-            CurrentDateLabel.textColor = UIColor.white
             DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
                 self.quizCategoriesViewModel.GoToQuiz(quiz: category.base, category: category)
             }
