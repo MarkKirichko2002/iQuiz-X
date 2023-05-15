a7a004d3783acf55ac3390db8a2d9725ce87d37f
MarkDestroyer <markkir123@gmail.com>
2023-05-15 23:00:44 +0300

    update
    Refactored code.

---
+++ QUIZ/QUIZ/Resources/Modules/MVVM Modules/Players Module/Players List MVVM/ViewModel/PlayersListViewViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  PlayersListViewViewModel.swift
+//  QUIZ
+//
+//  Created by Марк Киричко on 15.05.2023.
+//
+
+import UIKit
+import Combine
+
+protocol PlayersListViewViewModelDelegate: AnyObject {
+    func playersLoaded()
+}
+
+class PlayersListViewViewModel: NSObject {
+    
+    public weak var delegate: PlayersListViewViewModelDelegate?
+    private var players = [Player]()
+    
+    func GetPlayers() {
+        FirebaseManager().LoadPlayers { players in
+            DispatchQueue.main.async {
+                self.players = players
+                self.delegate?.playersLoaded()
+            }
+        }
+    }
+}
+
+extension PlayersListViewViewModel: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return players.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: PlayerTableViewCell.identifier, for: indexPath) as? PlayerTableViewCell else {
+            fatalError("Unsupported cell")
+        }
+        cell.configure(players: players[indexPath.row])
+        return cell
+    }
+}
