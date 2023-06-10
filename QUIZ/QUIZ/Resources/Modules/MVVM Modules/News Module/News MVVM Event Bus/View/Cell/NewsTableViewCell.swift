9bf128e56d5432f0bd4b10973ce6b8f53afbf261
MarkDestroyer <markkir123@gmail.com>
2023-06-10 18:55:07 +0300

    update
    Added shake gesture to generate random news.

--- QUIZ/QUIZ/Resources/Modules/MVVM Modules/News Module/News MVVM Event Bus/View/Cell/NewsTableViewCell.swift
+++ QUIZ/QUIZ/Resources/Modules/MVVM Modules/News Module/News MVVM Event Bus/View/Cell/NewsTableViewCell.swift
@@ -19,7 +19,7 @@ class NewsTableViewCell: UITableViewCell {
     @IBOutlet weak var DescriptionLabel: UILabel!
     
     func configure(news: Article) {
-        NewsImage.sd_setImage(with: URL(string: news.urlToImage ?? ""))
+        NewsImage.sd_setImage(with: URL(string: news.urlToImage ?? "https://media.istockphoto.com/id/1175387759/vector/camera-icon.jpg?s=612x612&w=0&k=20&c=paC1ZkU31dH2B5epXqT_cYOyca5uqh4v0WpFUldCUBE="))
         NewsImage.sound = "newspaper.mp3"
         TitleLabel.text = news.title ?? ""
         DescriptionLabel.text = news.description ?? ""
